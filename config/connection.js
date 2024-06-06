df529886a30d1d5081c89d33ff5c550edb5a04e9
Peachplace Deisler <avadeisler@gmail.com>
2024-06-06 13:42:46 -0500

    models and server

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+mongoose.connect(process.nextTick.MONGODB_URI ||
+    'mongodb://localhost:27017/NoSQLsocials/', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    module.exports = mongoose.connection
